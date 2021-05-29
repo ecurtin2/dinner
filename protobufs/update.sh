e42793a5e5699431727b670edd1bf20c70e12d79
ecurtin2 <ecurtin2@illinois.edu>
2021-05-29 15:22:14 -0500

    update debug recipe repo on frontent with embeddings

--- protobufs/update.sh
+++ protobufs/update.sh
@@ -3,3 +3,4 @@ protoc --python_betterproto_out=/build/python recipe.proto
 
 mkdir -p /build/typescript
 protoc --ts_proto_out=/build/typescript --ts_proto_opt=esModuleInterop=true --ts_proto_opt=outputClientImpl=grpc-web recipe.proto
+echo built typescript protofiles
